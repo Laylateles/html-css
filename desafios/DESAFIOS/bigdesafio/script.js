befd9ac1be5ae9c8ae2bf5f3b01ec80125a33e25
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-12 20:35:33 -0300

    terminei media queries

--- desafios/DESAFIOS/bigdesafio/script.js
+++ desafios/DESAFIOS/bigdesafio/script.js
@@ -1,12 +1,7 @@
 function cadastrar(){
-    var nome = window.document.getElementById('nome')
-    var sobrenome = window.document.getElementById('sob')
     var CelEmail = window.document.getElementById('celemail')
     var senha = window.document.getElementById('senha')
     var nasci = window.document.getElementsByName('num')
     var mes = window.document.getElementsByName('mes')
     var ano = window.document.getElementsByName('ano')
-    if(nome == 0 ){
-        window.alert('Preencha todos os quesitos!')
-    }
-}
+} // sinto q preciso de mais conhecimento pois ainda n consigo fazer uma boa validação com facilidade!!!
