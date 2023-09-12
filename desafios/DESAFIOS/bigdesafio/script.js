ba63eeba12ced17d765b019525b3bf861e2caa45
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-11 21:29:35 -0300

    finalizando

--- desafios/DESAFIOS/bigdesafio/script.js
+++ desafios/DESAFIOS/bigdesafio/script.js
@@ -1,3 +1,12 @@
 function cadastrar(){
-    window.alert('Cadastrado!')
+    var nome = window.document.getElementById('nome')
+    var sobrenome = window.document.getElementById('sob')
+    var CelEmail = window.document.getElementById('celemail')
+    var senha = window.document.getElementById('senha')
+    var nasci = window.document.getElementsByName('num')
+    var mes = window.document.getElementsByName('mes')
+    var ano = window.document.getElementsByName('ano')
+    if(nome == 0 ){
+        window.alert('Preencha todos os quesitos!')
+    }
 }
